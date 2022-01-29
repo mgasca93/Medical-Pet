04c508319d8274e28d3ce2ec58aa533264b14116
Mario A. Gasca <web@mariogasca.com>
2022-01-29 17:49:45 -0600

    Se añadio el archivo de funciones de ayuda para el sistema

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,15 @@
 <?php
 
 function isItemActive($routeName){
-    return ($routeName == $_SERVER['QUERY_STRING']) ? 'primary_txt fw-bold' : '';
+    return ($routeName == $_SERVER['QUERY_STRING']) ? 'item-active' : '';
+}
+
+function setTitle($title = ''){
+    $pageTitle = 'Medical Pet';
+
+    if(strlen($title) > 0 && isset($title)){
+        $pageTitle .= ' | ' . $title;
+    }
+
+    return $pageTitle;
 }
