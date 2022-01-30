497ea137a22d0290078a975167c14b8e2be1535d
Mario A. Gasca <web@mariogasca.com>
2022-01-30 12:37:24 -0600

    Se añadieron los helpers para la validacion de autenticacion

--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,21 @@
 <?php
 
+/**
+ * Valida que la ruta que se recibe como parametro
+ * coincida con la ruta actual
+ *
+ * @return string
+ */
 function isItemActive( $routeName ) {
     return ( $routeName == $_SERVER['QUERY_STRING'] ) ? 'item-active' : '';
 }
 
+/**
+ * Concatena el titulo de la página con el 
+ * nombre de la tienda y lo retorna
+ *
+ * @return string
+ */
 function setTitle( $title = '' ) {
     $pageTitle = 'Medical Pet';
 
@@ -14,11 +26,54 @@ function setTitle( $title = '' ) {
     return $pageTitle;
 }
 
+/**
+ * Esta funcion me permite debuggear cualquier
+ * tipo de datos y detiene la ejecución de la 
+ * aplicacion
+ *
+ * @return <pre></pre>
+ */
 function debug( $data ) {
     echo "<pre>";print_r($data);echo"</pre>";exit();
 }
 
+/**
+ * Esta funcion me permite redireccionar a la ruta
+ * que recibe como parametro
+ *
+ * @return void
+ */
 function redirect( $url ) {
     header( "location: $url" );    
     die();
+}
+
+/**
+ * Valido si existe una session activa
+ * y retorno un flag
+ *
+ * @return bool
+ */
+function auth() {
+    $flag = false;
+    if( \Horus\app\Services\SessionUp::exists() ) : 
+        $flag = true;
+    endif; 
+
+    return $flag;
+}
+
+/**
+ * Valido si existe una session activa
+ * y retorno un flag
+ *
+ * @return bool
+ */
+function guest() {
+    $flag = true;
+    if( \Horus\app\Services\SessionUp::exists() ) : 
+        $flag = false;
+    endif; 
+
+    return $flag;
 }
