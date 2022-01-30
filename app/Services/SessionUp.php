ef13bda3aacb576b0f8ec39295c32790cfb5cd9a
Mario A. Gasca <web@mariogasca.com>
2022-01-29 19:11:23 -0600

    Se creo el servicio para levantar/bajar las sessiones del usuario

---
+++ app/Services/SessionUp.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Horus\App\Services;
+
+class SessionUp{
+
+
+    /**
+     * Esta funcion recibe un arreglo con los valores
+     * para establecer las session
+     */
+    public function up(array $sessionsData){
+        foreach($sessionsData as $clave => $valor){
+            $_SESSION[$clave] = $valor;
+        }
+    }
+
+    /**
+     * Esta funcion recibe como argumento la clave de la session
+     * en caso de ser establecida retorn todas las sessiones existen
+     */
+    public function get(string $clave = ''){
+        if(strlen($clave) == 0){
+            return $_SESSION;
+        }else{
+            return $_SESSION[$clave];
+        }
+    }
+
+    /*
+    * Esta funcion se encarga de verificar 
+    * si existen sessiones activas
+    */
+    public function sessionExists(){
+        if(isset($_SESSION) && count($_SESSION) > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    /*
+    * Esta funcion se encarga de destruir 
+    * todas las sessiones
+    */
+    public function down(){
+        session_unset();
+        session_destroy();
+    }
+}
