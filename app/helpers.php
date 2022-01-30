020873b7fd13160f173078039cc124833f9ce404
Mario A. Gasca <web@mariogasca.com>
2022-01-30 01:49:31 -0600

    Se añdio el helper para el redireccionamiento

--- app/helpers.php
+++ app/helpers.php
@@ -1,15 +1,24 @@
 <?php
 
-function isItemActive($routeName){
-    return ($routeName == $_SERVER['QUERY_STRING']) ? 'item-active' : '';
+function isItemActive( $routeName ) {
+    return ( $routeName == $_SERVER['QUERY_STRING'] ) ? 'item-active' : '';
 }
 
-function setTitle($title = ''){
+function setTitle( $title = '' ) {
     $pageTitle = 'Medical Pet';
 
-    if(strlen($title) > 0 && isset($title)){
+    if( strlen( $title ) > 0 && isset( $title )) {
         $pageTitle .= ' | ' . $title;
     }
 
     return $pageTitle;
+}
+
+function debug( $data ) {
+    echo "<pre>";print_r($data);echo"</pre>";exit();
+}
+
+function redirect( $url ) {
+    header( "location: $url" );    
+    die();
 }
