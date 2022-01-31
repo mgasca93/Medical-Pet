fffdc8ccfbe60241fabac408d9056c191ecec38e
Mario A. Gasca <web@mariogasca.com>
2022-01-30 18:38:02 -0600

    Se quito de forma temporal la funcionalidad del servicio de envio de email de MailChimp

---
+++ app/Services/MailSend.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Horus\App\Services;
+
+use Horus\Config\Env;
+
+class MailSend
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function set(string $email = 'est.i.i.mariogasca@outlook.es'){
+
+        
+    }
+
+}
