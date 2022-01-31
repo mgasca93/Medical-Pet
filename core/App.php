6e26b22143d5bbfdd1ef899267eeb963accc8487
Mario A. Gasca <web@mariogasca.com>
2022-01-30 18:36:31 -0600

    Se añadieron las rutas de registro

--- core/App.php
+++ core/App.php
@@ -22,10 +22,14 @@ class App{
         $router->add('logout', ['controller' => 'LoginController', 'action' => 'logout']);
 
         $router->add('dashboard', ['controller' => 'Dashboard\\DashboardController', 'action' => 'index']);
-        $router->add('registro', ['controller' => 'PagesController', 'action' => 'register']);
-        
+        $router->add('register', ['controller' => 'PagesController', 'action' => 'register']);
+        $router->add('register/validate', ['controller' => 'RegisterController', 'action' => 'index']);
+        $router->add('register/validate/email', ['controller' => 'RegisterController', 'action' => 'validateEmail']);
+        $router->add('register/validate/username', ['controller' => 'RegisterController', 'action' => 'validateUsername']);
+
         #Se define la estructura de la ruta
         $router->add('{controller}/{action}');
+        
         $router->dispatch($_SERVER['QUERY_STRING']);
     }
 
